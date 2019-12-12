928d4950d625f17c4fc4b6a55d566ed2f73f8160
jessicaeverard <jessica@communitycodingclub.org.uk>
2019-12-12 12:58:59 +0000

    landing page

--- project1.php
+++ project1.php
@@ -5,19 +5,53 @@
 </head>
 <body>
 
-	<h1>Reward Scheme.</h1>
+	<h1 class = 'title'>RewardMe.</h1>
 
 	<style>
 		body {
 			background-color: #1a1a1d
 		}
-		h1 {
+		h1.title {
 			font-family: verdana;
 			color: #c3073f;
-			font-size: 40px;
+			font-size: 50px;
+		}
+		.box {
+			font-size: 35px;
+			height: 100px;
+			width: 500px;
+			background-color: #4e4e50;
+			margin: 1% auto;
+			height: 400px;
+			text-align: center;
+			font-family: verdana;
+			color: #1a1a1d;
+		}
+		.button {
+			background-color: #c3073f;
+			color: #1a1a1d;
+			font-family: verdana;
+			font-size: 50px;
+			border-radius: 4px;
+			cursor: pointer;
+			width: 300px;
+			padding: 15px;
+		}
+		
+		.button:hover {
+			background-color: #1a1a1d; 
+			color: #c3073f;
+			font-family: verdana;
+			font-size: 50px;
 		}
 	</style>
-	
-
+	<div class = 'box'>
+		<h1> Welcome. </h1>
+		<div>
+			<button class = 'button'> Log - in </button>
+			<p> </p>
+			<button class = 'button'> Register </button>
+		</div>
+	</div>
 </body>
 </html>
