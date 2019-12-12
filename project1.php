523b07963074bad5cf4efe6390f12d3eaa85ffed
jessicaeverard <jessica@communitycodingclub.org.uk>
2019-12-12 09:47:52 +0000

    just the title

---
+++ project1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Reward</title>
+</head>
+<body>
+
+	<h1>Reward Scheme.</h1>
+
+	<style>
+		body {
+			background-color: #1a1a1d
+		}
+		h1 {
+			font-family: verdana;
+			color: #c3073f;
+			font-size: 40px;
+		}
+	</style>
+	
+
+</body>
+</html>
