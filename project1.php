25beb6f0e5950db144e0e5512d657e7bc611b044
jessicaeverard <jessica@communitycodingclub.org.uk>
2019-12-17 11:46:17 +0000

    updated landing page

--- project1.php
+++ project1.php
@@ -1,56 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Reward</title>
+	<title>RewardMe.</title>
 </head>
 <body>
 
 	<h1 class = 'title'>RewardMe.</h1>
 
 	<style>
+		
 		body {
-			background-color: #1a1a1d
+			background-color: #eae7dc;
+			margin:0;
 		}
-		h1.title {
+		.title {
+			margin-top: -5px;
 			font-family: verdana;
-			color: #c3073f;
-			font-size: 50px;
+			color: #e85a4f;
+			font-size: 70px;
 		}
 		.box {
 			font-size: 35px;
 			height: 100px;
 			width: 500px;
-			background-color: #4e4e50;
+			background-color: #d8c3a5;
 			margin: 1% auto;
 			height: 400px;
 			text-align: center;
 			font-family: verdana;
-			color: #1a1a1d;
+			color: #e98074;
+			border-radius: 4px;
 		}
 		.button {
-			background-color: #c3073f;
-			color: #1a1a1d;
+			background-color: #e98074;
+			color: #eae7dc;
 			font-family: verdana;
 			font-size: 50px;
-			border-radius: 4px;
+			border-radius: 8px;
 			cursor: pointer;
 			width: 300px;
 			padding: 15px;
 		}
 		
 		.button:hover {
-			background-color: #1a1a1d; 
-			color: #c3073f;
+			background-color: #eae7dc; 
+			color: #e98074;
 			font-family: verdana;
 			font-size: 50px;
+		}
+		a {
+			text-decoration: none;
+		}
+		a:visited{
+			color: #d8c3a5;
 		}
 	</style>
 	<div class = 'box'>
 		<h1> Welcome. </h1>
 		<div>
-			<button class = 'button'> Log - in </button>
+			<button class = 'button'><a href = 'login.php '> Log - in  </a></button>
 			<p> </p>
-			<button class = 'button'> Register </button>
+			<button class = 'button'><a href = 'register.php' >Register</a> </button>
 		</div>
 	</div>
 </body>
